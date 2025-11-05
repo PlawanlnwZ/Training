2a827d8b77542a347c7816a70a8424f16ea789b7
PlawanlnwZ <tichapornrodpan@gmail.com>
2025-11-05 16:11:15 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const path = require('path')
+app.use(express.static("public"))
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(__dirname,"public",'idk.html'))
+})
+app.listen(5500)
